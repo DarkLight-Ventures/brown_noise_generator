0874893d101ec2017f5a49af8f8752839abdb304
Kyle <kyle@darklightventures.com>
2023-05-07 10:40:51 -0400

    adding a mixer

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,32 @@ extern crate biquad;
 
 
 use biquad::*;
-use rand::Rng;
 use hound::WavWriter;
+use rand::Rng;
 use std::i16;
 use std::f32::consts::PI;
+
 
 fn main() {
     let duration_secs = 60;
     let sample_rate = 44100;
-    let output_file_b = "brown_noise.wav";
     let output_file_a = "white_noise.wav";
+    let output_file_b = "brown_noise.wav";
+    let output_file_c = "warbled15_brown_noise.wav";
+    let output_file_d = "warbled30_brown_noise.wav";
     let cutoff_frequency = 900.0; // Hz
 
-    let brown_noise_samples = generate_brown_noise(duration_secs, sample_rate);
+    let brown_noise_samples = generate_white_noise(duration_secs, sample_rate);
     let filtered_samples = apply_low_pass_filter(&brown_noise_samples, cutoff_frequency, sample_rate);
-    write_wav_samples(output_file_a, sample_rate, &brown_noise_samples);
+    let warbled_samples_15 = apply_warble_effect(&filtered_samples, 0.15, sample_rate, 0.5);
+    let warbled_samples_30 = apply_warble_effect(&warbled_samples_15, 0.15, sample_rate, 0.5);
+
     write_wav_samples(output_file_b, sample_rate, &filtered_samples);
+    write_wav_samples(output_file_a, sample_rate, &brown_noise_samples);
+    write_wav_samples(output_file_c, sample_rate, &warbled_samples_15);
+    write_wav_samples(output_file_d, sample_rate, &warbled_samples_30);
 }
 
-
-// ...
-
-
-// ...
 
 fn apply_low_pass_filter(samples: &[i16], cutoff_frequency: f32, sample_rate: u32) -> Vec<i16> {
     let mut filtered_samples = vec![0i16; samples.len()];
@@ -45,11 +48,11 @@ fn apply_low_pass_filter(samples: &[i16], cutoff_frequency: f32, sample_rate: u3
 }
 
 
-fn apply_warble_effect(samples: &[i16], lfo_frequency: f64, sample_rate: u32, depth: f32) -> Vec<i16> {
+fn apply_warble_effect(samples: &[i16], lfo_frequency: f32, sample_rate: u32, depth: f32) -> Vec<i16> {
     let mut warbled_samples = vec![0i16; samples.len()];
-    let lfo_increment = 2.0 * PI * lfo_frequency / (sample_rate as f64);
+    let lfo_increment = 2.0 * PI * lfo_frequency / (sample_rate as f32);
 
-    let mut lfo_phase = 0.0;
+    let mut lfo_phase: f32 = 0.0;
 
     for (input_sample, output_sample) in samples.iter().zip(warbled_samples.iter_mut()) {
         let lfo = (lfo_phase.sin() + 1.0) * 0.5 * depth + (1.0 - depth);
@@ -61,27 +64,51 @@ fn apply_warble_effect(samples: &[i16], lfo_frequency: f64, sample_rate: u32, de
 }
 
 
-
-fn generate_brown_noise(duration_secs: u32, sample_rate: u32) -> Vec<i16> {
+fn generate_white_noise(duration_secs: u32, sample_rate: u32) -> Vec<i16> {
     let num_samples = duration_secs * sample_rate;
     let mut rng = rand::thread_rng();
 
-    let mut brown_noise = Vec::with_capacity(num_samples as usize);
+    let mut white_noise = Vec::with_capacity(num_samples as usize);
     let mut current_sample = 0.0;
 
     for _ in 0..num_samples {
-        let white_noise = rng.gen_range(-1.0..1.0);
-        current_sample += white_noise;
+        let noise = rng.gen_range(-1.0..1.0);
+        current_sample += noise;
 
         // Normalize the sample to prevent overflow
         current_sample *= 0.5;
 
         let sample_int = (current_sample * i16::MAX as f32).round() as i16;
-        brown_noise.push(sample_int);
+        white_noise.push(sample_int);
     }
 
-    brown_noise
+    white_noise
 }
+
+
+fn mix_wav_samples(samples1: &[i16], samples2: &[i16], blend_factor: f32) -> Vec<i16> {
+    let length = samples1.len().max(samples2.len());
+    let mut mixed_samples = vec![0i16; length];
+
+    for i in 0..length {
+        let sample1 = if i < samples1.len() {
+            samples1[i] as f32
+        } else {
+            0.0
+        };
+
+        let sample2 = if i < samples2.len() {
+            samples2[i] as f32
+        } else {
+            0.0
+        };
+
+        mixed_samples[i] = (sample1 * (1.0 - blend_factor) + sample2 * blend_factor) as i16;
+    }
+
+    mixed_samples
+}
+
 
 fn write_wav_samples(output_file: &str, sample_rate: u32, samples: &[i16]) {
     let spec = hound::WavSpec {
